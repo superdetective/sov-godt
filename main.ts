ac0f38ad131f7aa1de69d466b4108ae8f937e817
superdetective <91897645+superdetective@users.noreply.github.com>
2021-10-11 19:08:49 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,49 @@
+input.onButtonPressed(Button.A, function () {
+    if (input.temperature() < 25) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        music.playMelody("C5 B A G F E D C ", 120)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        music.playMelody("C5 B C5 - C5 B C5 - ", 120)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
